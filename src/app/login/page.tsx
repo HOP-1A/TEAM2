646ca9c0df36f42b8dcca7c386601b6f4b6c8f9c
24HP2206 24HP2206 <24HP2206@G305-CC0244.local>
2025-02-15 13:41:03 +0800

    idk

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,6 +18,7 @@ import {
 import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
+<<<<<<< HEAD
   number: z.coerce
     .number()
     .min(10000000, {
@@ -35,6 +36,32 @@ const Login = () => {
       number: 0,
     },
   });
+=======
+  number: z.coerce.number().min(10000000, {
+    message: "Утасны дугаар буруу байна",
+  }).max(99999999, {
+    message: "Утасны дугаар буруу байна",
+  }),
+  password: z.string().min(4,{
+    message: 'Хамгийн багадаа 4 орон байна.'
+  })
+})
+
+
+
+const Login = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          number: 0,
+          password: ''
+        },
+      })
+     
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        const number = Number(values.number)
+    }
+>>>>>>> 7908b6b (idk)
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     const number = Number(values.number);
@@ -44,6 +71,7 @@ const Login = () => {
 
   return (
     <div className="flex w-[100vw] h-[90vh] justify-center items-center">
+<<<<<<< HEAD
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
@@ -69,6 +97,42 @@ const Login = () => {
           <Button onClick={() => router.push("/signup")}>Бүртгүүлэх</Button>
         </form>
       </Form>
+=======
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-4 w-[500px] rounded-md flex flex-col justify-center" style={{boxShadow:"rgba(0, 0, 0, 0.05) 0px 0px 16px"}}>
+                <div>Нэвтрэх</div>
+                  <FormField
+                  control={form.control}
+                  name="number"
+                  render={({ field }) => (
+                      <FormItem>
+                      <FormLabel>Утасны дугаар</FormLabel>
+                      <FormControl>
+                          <Input placeholder="123456**" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                      </FormItem>
+                  )}
+                  />
+                  <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                      <FormItem>
+                      <FormLabel>Нууц үг</FormLabel>
+                      <FormControl>
+                          <Input type="password" placeholder="****" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                      </FormItem>
+                  )}
+                  />
+                <Button type="submit">Нэвтрэх</Button>
+                <div className="flex w-[100%] justify-center">Эсвэл</div>
+                <Button onClick={() => router.push('/signup')}>Бүртгүүлэх</Button>
+            </form>
+        </Form>
+>>>>>>> 7908b6b (idk)
     </div>
   );
 };
