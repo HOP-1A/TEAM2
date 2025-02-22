a5d27ee57b66e627aa01b22bc5729bf0bf18f216
24HP4966 24HP4966 <24HP4966@admins-iMac.local>
2025-02-22 11:33:09 +0800

    added prodcut card component

---
+++ src/app/_components/CardSmall.tsx
@@ -0,0 +1,27 @@
+import { Card } from "@/components/ui/card";
+import { Heart } from "lucide-react";
+
+export const CardSmall = () => {
+  return (
+    <>
+      <Card className="w-[175px] h-[278px]">
+        <img
+          className="w-[175px] h-[175px] object-contain "
+          src="https://cdnp.cody.mn/spree/images/2854987/product/IMG_5555.jpeg"
+          alt=""
+        />
+        <hr />
+        <div className="p-2">
+          <div className="flex justify-between">
+            <p className="text-xs text-gray-500 ">1 өдрийн өмнө</p>
+            <Heart className="w-5 h-5" />
+          </div>
+          <div className="flex-col space-y-6 h-[66px]">
+            <p className="text-[14px] text-gray-600 font-semibold">Дээл</p>
+            <p className="text-[16px] font-semibold">130,000 ₮</p>
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+};
