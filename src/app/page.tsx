c664b7e1e3962375e602291593b1ab618734d9d9
24HP3784 <24hp3784@admins-iMac.local>
2025-02-22 12:46:24 +0800

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
+import { CardSmall } from "./_components/CardSmall";
 import Carousel from "./components/carousel";
 export default function Home() {
 
   return (
     <div>
 <Carousel />
+<CardSmall/>
     </div>
   );
 }
